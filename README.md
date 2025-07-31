ab3c8f9c168f133892ebdf24331dd9e60e41a9e7
Ilpo Viertola <ilpo.viertola@tuni.fi>
2025-07-31 12:02:23 +0300

    add evaluation data handling

--- README.md
+++ README.md
@@ -1,5 +1,60 @@
-# SAGANet: Video Object Segmentation-aware Audio Generation
+# SAGANet: Video Object Segmentation-Aware Audio Generation
 
-The official implementation of SAGANet (in proceedings).
+[Ilpo Viertola](https://scholar.google.com/citations?user=gGWNg4EAAAAJ&hl=en), [Vladimir Iashin](https://scholar.google.com/citations?user=rh8_sSkAAAAJ&hl=en), and [Esa Rahtu](https://scholar.google.com/citations?user=SmGZwHYAAAAJ&hl=en)
+
+[[Project Page](saganet.notion.site)]
+
+DAGM German Conference on Pattern Recognition (GCPR) 2025.
 
 Thanks for the team of [MMAudio](https://github.com/hkchengrex/MMAudio). Our work is based on their codebase, licensed under the MIT License.
+
+## Cite
+
+```bibtex
+@inproceedings{poviertola2025saganet,
+  title={SAGANet: Video Object Segmentation-Aware Audio Generation},
+  author={Viertola, Ilpo and Iashin, Vladimir and Rahtu, Esa},
+  booktitle={DAGM German Conference on Pattern Recognition (GCPR)},
+  year={2025}
+}
+```
+
+## Installation
+
+Tested with Ubuntu 22.04.5.
+
+### 1. Install following dependencies to your virtual environment
+
+- Python 3.9
+- PyTorch 2.6
+
+### 2. Clone the repository
+
+```bash
+git clone git@github.com:ilpoviertola/SAGANet.git
+```
+
+### 3. Install the requirements
+
+```bash
+cd SAGANet
+pip install -r requirements.txt
+```
+
+All the necessat weights are downloaded automatically when you run the demo.
+
+## Demo
+
+Running the demo takes around 4.5 GB of GPU memory. Below is an example command. Explore the `demo.py` file for more options.
+
+```bash
+OMP_NUM_THREADS=1 torchrun --standalone --nproc_per_node=1 demo.py --variant saganet_small --video ./example/Vid_09_Jesus_tpt_vn_159092_164092.mp4 --mask_video ./example/Masks_02_vn_09_Jesus_159092_164092.mp4 --prompt violin --negative_prompt trumpet
+```
+
+## Segmented Music Solos (SMS) Dataset
+
+We propose a new dataset to facilitate training of video segmentation-aware audio models. Check out the [project page](https://saganet.notion.site/Segmented-Music-Solos-SMS-23e82e9d170080369505c222bda09447) for up-to-date information how to obtain the dataset.
+
+## TODO
+
+- [ ] Add on-the-fly segmentation mask generation from user prompts.
--- training/load_sms.py
+++ training/load_sms.py
@@ -4,15 +4,18 @@ First download the raw data following the instructions in the following datasets
 MUSIC21 (https://github.com/roudimit/MUSIC_dataset)
 AVSSBench (https://github.com/OpenNLPLab/AVSBench)
 Solos (https://github.com/JuanFMontesinos/Solos)
+URMP (https://labsites.rochester.edu/air/projects/URMP.html)
 """
 
 import argparse
 from pathlib import Path
 import requests
+import typing as tp
 from shutil import move, copy2
 
 from tqdm import tqdm
 import ffmpeg
+import pandas as pd
 
 
 SAGANET_PUB_URL = (
@@ -41,6 +44,12 @@ def get_args():
         help="Path to the AVSSBench dataset directory",
     )
     parser.add_argument(
+        "--urmp",
+        type=str,
+        required=True,
+        help="Path to the URMP dataset directory (not used in this script, but required for consistency)",
+    )
+    parser.add_argument(
         "--move_files",
         action="store_true",
         help="Move files instead of copying them",
@@ -48,8 +57,8 @@ def get_args():
     parser.add_argument(
         "--output_dir",
         type=str,
-        default="./sms",
-        help="Output directory for SMS clips",
+        default="./data",
+        help="Output directory for datasets",
     )
     return parser.parse_args()
 
@@ -88,31 +97,25 @@ def handle_video_file(video_path: Path, out_path: Path, start_msec: int, end_mse
 
 
 def read_csv(file_path: Path):
-    import pandas as pd
-
     if not file_path.exists():
         raise FileNotFoundError(f"File {file_path} does not exist.")
     return pd.read_csv(file_path)
 
 
-def main():
-    args = get_args()
-    solos_path = Path(args.solos)
-    assert solos_path.exists(), f"Solos path {solos_path} does not exist."
-    music21_path = Path(args.music21)
-    assert music21_path.exists(), f"Music21 path {music21_path} does not exist."
-    avssbench_path = Path(args.avssbench)
-    assert avssbench_path.exists(), f"AVSSBench path {avssbench_path} does not exist."
-    output_dir = Path(args.output_dir)
-    output_dir.mkdir(parents=True, exist_ok=True)
+def read_tsv(file_path: Path):
+    if not file_path.exists():
+        raise FileNotFoundError(f"File {file_path} does not exist.")
+    return pd.read_csv(file_path, sep="\t")
 
-    # Download SMS metadata and generated masks
+
+def download_files(output_dir: Path):
     file_list = download(output_dir / "files.txt")
     files_to_download = file_list.read_text().splitlines()
+    files_to_download = [f for f in files_to_download if not f.endswith(".pth")]
     print(f"Downloading {len(files_to_download)} files...")
     for file_name in tqdm(
         files_to_download,
-        desc="Downloading SMS",
+        desc="Downloading files",
         unit="file",
         total=len(files_to_download),
     ):
@@ -120,22 +123,26 @@ def main():
         if not file_path.exists():
             file_path.parent.mkdir(parents=True, exist_ok=True)
             download(file_path, f"{SAGANET_PUB_URL}/{file_name}")
+
+
+def handle_sms(
+    solos_path: Path,
+    music21_path: Path,
+    avssbench_path: Path,
+    output_dir: Path,
+    act: tp.Callable,
+):
     assert (
         output_dir / "sms" / "metadata.csv"
     ).exists(), "Metadata file not found in the SMS directory."
 
-    # Create symlinks or move the files
-    move_files = args.move_files
-    if move_files:
-        print("NOTE: We are moving files instead of copying them!!")
-        act = move
-    else:
-        act = copy2
     metadata = read_csv(output_dir / "sms" / "metadata.csv")
     metadata = metadata.reset_index()
 
     output_dir = output_dir / "sms"
-    for _, row in metadata.iterrows():
+    for i, row in metadata.iterrows():
+        if i == 5:
+            break
         if row["subset"] == "solos":
             inp = solos_path
         elif row["subset"] == "avs-semantic":
@@ -148,24 +155,106 @@ def main():
         processed = False
         fn = row["YTID"]
         fp = list(inp.rglob(f"{fn}.mp4"))
-        fp = fp[0] if fp else None
+        fp = fp[0] if fp else None  # type: ignore
         if not fp:
             fn = row["filename"]
             fp = list(inp.rglob(f"{fn}.mp4"))
-            fp = fp[0] if fp else None
+            fp = fp[0] if fp else None  # type: ignore
             processed = True
         if not fp:
             print(f"File not found for {row['YTID']} ({row['filename']})")
             continue
         if not processed:
             handle_video_file(
-                fp,
+                fp,  # type: ignore
                 output_dir / "data" / row["redefined_label"],
                 row["start_msec"],
                 row["end_msec"],
             )
         else:
-            act(fp, output_dir / fp.name)
+            act(fp, output_dir / "data" / row["redefined_label"] / fp.name)  # type: ignore
+
+
+def handle_urmp(
+    urmp_path: Path,
+    output_dir: Path,
+    act: tp.Callable,
+):
+    assert (
+        output_dir / "urmp" / "metadata.tsv"
+    ).exists(), "Metadata file not found in the URMP directory."
+
+    metadata = read_tsv(output_dir / "urmp" / "metadata.tsv")
+    metadata = metadata.reset_index()
+
+    output_dir = output_dir / "urmp"
+    for i, row in metadata.iterrows():
+        if i == 5:
+            break
+        processed = False
+        p_dir, v_id = row["video_id"].split("/")
+        matches = list(urmp_path.rglob(f"{v_id}.mp4"))
+        if matches:
+            assert len(matches) == 1, f"Multiple matches found for {v_id}"
+            fp = matches[0]
+            processed = True
+        else:
+            v_id = "_".join(v_id.split("_")[:-2])
+            matches = list(urmp_path.rglob(f"{v_id}.mp4"))
+            if matches:
+                assert len(matches) == 1, f"Multiple matches found for {v_id}"
+                fp = matches[0]
+            else:
+                print(f"File not found for {row['video_id']}")
+                continue
+        if not processed:
+            handle_video_file(
+                fp,
+                output_dir / "data" / p_dir,
+                row["s_ts"],
+                row["e_ts"],
+            )
+        else:
+            act(fp, output_dir / "data" / p_dir / fp.name)
+
+
+def main():
+    args = get_args()
+    solos_path = Path(args.solos)
+    assert solos_path.exists(), f"Solos path {solos_path} does not exist."
+    music21_path = Path(args.music21)
+    assert music21_path.exists(), f"Music21 path {music21_path} does not exist."
+    avssbench_path = Path(args.avssbench)
+    assert avssbench_path.exists(), f"AVSSBench path {avssbench_path} does not exist."
+    urmp_path = Path(args.urmp)
+    assert urmp_path.exists(), f"URMP path {urmp_path} does not exist."
+    output_dir = Path(args.output_dir)
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    # Download SMS/URMP metadata and generated masks
+    download_files(output_dir)
+
+    # Create symlinks or move the files
+    move_files = args.move_files
+    if move_files:
+        print("NOTE: We are moving files instead of copying them!!")
+        act = move
+    else:
+        act = copy2
+
+    handle_sms(
+        solos_path,
+        music21_path,
+        avssbench_path,
+        output_dir,
+        act=act,
+    )
+
+    handle_urmp(
+        urmp_path,
+        output_dir,
+        act=act,
+    )
 
 
 if __name__ == "__main__":
